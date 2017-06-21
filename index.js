63c18411afc5065078c6022b4699c1fb1644d530
Andiedie <zchangan@163.com>
2017-06-21 17:59:06 +0800

    结构确定

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const config = require('./config');
-const {login, init} = require('./utils');
+const {login, init, check, select} = require('./utils');
+const {log} = require('./lib');
 
 main();
 async function main () {
   let sid = await login(config.netid, config.password);
-  await init(sid);
+  let xkjdszid = await init(sid);
+  await check(xkjdszid, select);
+  log.info('完成');
 }
