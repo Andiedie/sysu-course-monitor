87bf3285f95924af5f178ef91d6742a955e18532
Andiedie <zchangan@163.com>
2017-06-24 17:13:37 +0800

    improve code

--- index.js
+++ index.js
@@ -11,27 +11,29 @@ async function main () {
     sid = await login(config.netid, config.password);
   } catch (e) {
     console.log(e);
-    console.log('登录失败...');
+    return console.log('登录失败...');
   }
 
   try {
     xkjdszids = await init(sid, config.interval);
   } catch (e) {
     console.log(e);
-    console.log('进入选课系统失败...');
+    return console.log('进入选课系统失败...');
   }
 
   let checker = new Checker(sid, xkjdszids, config.interval, config.settings);
-  checker.start();
-
   let selector = new Selector();
-  checker.on('selectable', selector.select);
-  checker.on('finish', () => console.log('完成！'));
 
-  selector.on('success', inform.bind(null, '成功'));
-  selector.on('fail', inform.bind(null, '失败'));
-  selector.on('finish', () => {
-    let json = JSON.stringify(config.settings, null, 2);
-    fs.writeFile('./config/settings.json', json);
-  });
+  checker
+    .on('selectable', selector.select)
+    .on('finish', () => console.log('完成！'))
+    .start();
+
+  selector
+    .on('success', inform.bind(null, '成功'))
+    .on('fail', inform.bind(null, '失败'))
+    .on('finish', () => {
+      let json = JSON.stringify(config.settings, null, 2);
+      fs.writeFile('./config/settings.json', json);
+    });
 }
