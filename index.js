3d72f8fab95ef8888670d499c5313b45e1275d40
Andiedie <zchangan@163.com>
2017-06-30 13:40:59 +0800

    perfect initialzation log

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ async function main () {
   let outputConfig = '当前配置：\n';
   outputConfig += `查询间隔：${config.interval}毫秒\n`;
   enables.forEach(current => {
-    outputConfig += `\n-- ${current.type}：
-                     ---- 替换课程：${current.replace ? current.replaceName : '无'}
-                     ---- 目标课程：
-                     ------ `;
-    outputConfig += current.targets.map(({name}) => name instanceof RegExp ? name.source : name).join('\n------ ');
+    outputConfig += `\n ${current.type}：
+                     ----- 替换课程：${current.replace ? current.replaceName : '无'}
+                     ----- 目标课程：
+                     -------------- `;
+    outputConfig += current.targets.map(({name}) => name instanceof RegExp ? name.source : name).join('\n-------------- ');
     outputConfig += '\n';
   });
   log(outputConfig);
