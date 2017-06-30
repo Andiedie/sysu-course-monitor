541d32ef1062949b54a24ec0ee0ff0ed76e1e8b1
Andiedie <zchangan@163.com>
2017-06-30 20:15:33 +0800

    unify line break to LF

--- config/index.js
+++ config/index.js
@@ -1,17 +1,15 @@
-const secret = require('./secret');
-
 // NetID账号
-exports.netid = secret.netid;
+exports.netid = '';
 // NetID密码
-exports.password = secret.password;
+exports.password = '';
 // 刷新间隔 毫秒
 exports.interval = 3000;
 
 // 是否开启微信推送
 exports.wechatInform = true;
 // 测试号appid
-exports.appid = secret.appid;
+exports.appid = '';
 // 测试号appsecret
-exports.appsecret = secret.appsecret;
+exports.appsecret = '';
 // 模板消息id
-exports.template_id = secret.template_id;
+exports.template_id = '';
